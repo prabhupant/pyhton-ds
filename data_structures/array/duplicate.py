8f318a39d4f9c2cce0b2b2a938534413c6de0fb5
cglasser444 <cglasser44@gmail.com>
2024-10-30 21:03:30 -0400

    tested changing directions slightly

--- data_structures/array/duplicate.py
+++ data_structures/array/duplicate.py
@@ -1,11 +1,11 @@
-# Find duplicate in an array of integers given that the integers are in random order and 
+# You will find a duplicate in an array of integers given that the integers are in random order and 
 # not necessarily each integer i is 0 <= i <= N where N = length of array
 
 # Solution - use tortoise and hare algorithm. The tortoise pointer moves slower while the hare pointer
 # moves faster
 
 # Note: this array will always contain a duplicate number due to the pigeonhole principle. You are trying to fit
-# N different numbers in an array of size N - 1 so one number will be repeated 
+# N different numbers in an array of size N - 1 so one number will be repeated
 
 def duplicate(arr):
     tortoise = arr[0]
